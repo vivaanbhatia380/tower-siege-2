4bb13e70296d7532f780b7122760d5f7f9925b6c
vivaanbhatia380 <71877612+vivaanbhatia380@users.noreply.github.com>
2020-11-24 20:34:39 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,14 @@
+class Rope{
+block8=new Block(330,235,30,40)
+block9=new Block(360,235,30,40)
+block10=new Block(390,235,30,40)
+block11=new Block(410,235,30,40)
+block12=new Block(420,235,30,40)
+
+// level 3
+block13=new Block(360,195,30,40)
+block14=new Block(390,195,30,40)
+block15=new Block(420,195,30,40)
+// top
+block16=new Block(390,155,30,40)
+}
